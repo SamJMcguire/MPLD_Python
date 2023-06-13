28d44a1b60ed49e6141840a35197014bc3c2f033
SamJMcguire <45981861+SamJMcguire@users.noreply.github.com>
2023-06-13 21:43:52 +0000

    I have created a basic program to insult people

--- demo.py
+++ demo.py
@@ -1,1 +1,12 @@
+"""First program
+Sam McGuire
+This is a demo for MPLD"""
 
+print("Hello world")
+user_name = input("What is your name:  ")
+print("Hello, ", user_name)
+
+if user_name == "Sam":
+    print(user_name, ", that is a cool name!")
+else:
+    print(user_name, "who the hell names their kid", user_name, "!")
