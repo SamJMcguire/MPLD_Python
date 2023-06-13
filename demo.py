2d5bdd7446a4cea8d01ea3fa563f23a4e4ba49ee
SamJMcguire <45981861+SamJMcguire@users.noreply.github.com>
2023-06-13 22:06:23 +0000

    definitions - hello world module and bob module

--- demo.py
+++ demo.py
@@ -2,11 +2,19 @@
 Sam McGuire
 This is a demo for MPLD"""
 
-print("Hello world")
-user_name = input("What is your name:  ")
-print("Hello, ", user_name)
+def hello_world():
+    print("Hello world")
+    user_name = input("What is your name:  ")
+    print("Hello, ", user_name)
 
-if user_name == "Sam":
-    print(user_name, ", that is a cool name!")
-else:
-    print(user_name, "who the hell names their kid", user_name, "!")
+    if user_name == "Sam":
+        print(user_name, ", that is a cool name!")
+    else:
+        print(user_name, "who the hell names their kid", user_name, "!")
+
+def bob_module():
+    print("Boo!")
+
+
+bob_module()
+hello_world()
